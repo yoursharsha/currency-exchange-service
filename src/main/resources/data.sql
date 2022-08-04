bfb7768b7d23e9594481adc31edd008088d416d8
yoursharsha <41571745+yoursharsha@users.noreply.github.com>
2022-08-04 15:13:32 +0530

    RuppeTo70

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple)
-values(10001,'USD','INR',80);
+values(10001,'USD','INR',70);
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple)
 values(10002,'EUR','INR',75);
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple)
